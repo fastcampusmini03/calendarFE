72cf72eb3bb0e8284cecac7c69c12da28c473c1b
dokimion24 <leecy3610@gmail.com>
2023-05-03 18:32:40 +0900

    ✨ loginPage, signupPage 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,19 +1,19 @@
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "../pages/main";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Main from '../pages/main'
+import LoginPage from '../pages/loginPage'
+import SignupPage from '../pages/signupPage'
 
 function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main/>} />
-        {/* <Route path="/login" element={<Login />} /> */}
-        {/* <Route path="/signup" element={<Signup />} /> */}
-        <Route path="*" element={<>notFound</>} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
+                <Route path="*" element={<>notFound</>} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default Router;
+export default Router
