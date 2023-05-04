d5a5a7a16bee78bc4ad2c85b923556122b2d4902
jyc-coder <jyc4648@gmail.com>
2023-05-04 20:06:26 +0900

    :hammer: Router 추가 (관리자페이지, 유저관리페이지)
    라우터 추가 
    - AdminPage
    - UserListPage

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,6 +3,8 @@ import Main from '../pages/main'
 import SignupPage from '../pages/SignupPage'
 import LoginPage from '../pages/LoginPage'
 import MyPage from '../pages/MyPage'
+import AdminPage from '../pages/AdminPage'
+import UserListPage from '../pages/UserListPage'
 
 function Router() {
   return (
@@ -12,6 +14,8 @@ function Router() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/admin/user" element={<UserListPage />} />
         <Route path="*" element={<>notFound</>} />
       </Routes>
     </BrowserRouter>
