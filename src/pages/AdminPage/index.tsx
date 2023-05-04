27aa594587eed7a5824f3e5c4aa9c76d4c2fd144
jyc-coder <jyc4648@gmail.com>
2023-05-04 19:32:41 +0900

    :lipstick: 스타일 속성 변경및, UI추가(AdminPage, style)
    - AdminPage Grid 배율 변경
    
    
    - style.ts 파일 내부에 ui컴포넌트 추가
       - ListPapper : 일반 사용자로부터 전달된 연차/당직 리스트
       - Search : 검색창 컨테이너 div
       - SearchIconWrapper : 검색 아이콘을 감싸는 wrapper
       - StyledInputBase : 검색 입력하는 input 컴포넌트

--- src/pages/AdminPage/index.tsx
+++ src/pages/AdminPage/index.tsx
@@ -6,16 +6,17 @@ import { DatesPayload } from '../../types/dates'
 import Grid from '@mui/material/Grid'
 
 function AdminPage() {
+  //4, 5, 6      // 5, 6, 7
   const { data: dates, isLoading } = useQuery<DatesPayload[]>('dates', getDates)
   if (isLoading || dates === undefined) {
     return <div>로딩중...</div>
   }
   return (
     <Grid container spacing={1}>
-      <Grid item xs={9}>
+      <Grid item xs={8}>
         <CalendarUI view={'month'} dates={dates} />
       </Grid>
-      <Grid item xs={3}>
+      <Grid item xs={4}>
         <AnnualDutyList dates={dates} />
       </Grid>
     </Grid>
