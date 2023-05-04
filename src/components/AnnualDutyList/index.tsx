2f3c6c92879e8d3e7fcd3d8d4f0909c457a78ad9
jyc-coder <jyc4648@gmail.com>
2023-05-04 20:01:41 +0900

    :lipstick: 연차/당직 관리 리스트 ui 구현
    - TypoGraphy 사용하여 제목 
    - ListItem을 사용하여 요청 리스트 목록을 렌더링

--- src/components/AnnualDutyList/index.tsx
+++ src/components/AnnualDutyList/index.tsx
@@ -1,22 +1,52 @@
+import List from '@mui/material/List'
 import { DatesPayload } from '../../types/dates'
+import Typography from '@mui/material/Typography'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemButton from '@mui/material/ListItemButton'
+import { Box, Button } from '@mui/material'
+import { textAlign } from 'styled-system'
 
 interface AdminPageProps {
   dates: DatesPayload[]
 }
 
 function AnnualDutyList({ dates }: AdminPageProps) {
+  const formatter = new Intl.DateTimeFormat('ko-KR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  })
   return (
     <div>
-      {dates.map((result: DatesPayload) => (
-        <div key={result.id}>
-          <div>{result.username}</div>
-          <div> {result.role}</div>
+      <Box>
+        <Typography
+          variant="h2"
+          color="initial"
+          style={{ whiteSpace: 'nowrap', textAlign: 'center' }}
+        >
+          연차 / 당직 관리
+        </Typography>
+      </Box>
 
-          <div>{result.title}</div>
-          <div>{result.start.toLocaleString('ko-KR')}</div>
-          <div>{result.start.toLocaleString('ko-KR')}</div>
-        </div>
-      ))}
+      <List>
+        {dates.map((result: DatesPayload) => (
+          <ListItem>
+            <ListItemText
+              primary={result.username + ' ' + result.role + ' ' + result.title}
+              secondary={
+                formatter.format(new Date(result.start)) +
+                ' - ' +
+                formatter.format(new Date(result.end))
+              }
+            />
+            <Button>승인</Button>
+            <Button variant="outlined">거부</Button>
+          </ListItem>
+        ))}
+      </List>
     </div>
   )
 }
