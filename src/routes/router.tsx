5c70d621a26433f619e4c6191d667d641237e989
dokimion24 <leecy3610@gmail.com>
2023-05-04 14:53:21 +0900

    ✨ MyPage컴포넌트 라우팅 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Main from '../pages/main'
 import SignupPage from '../pages/SignupPage'
 import LoginPage from '../pages/LoginPage'
+import MyPage from '../pages/MyPage'
 
 function Router() {
   return (
@@ -10,6 +11,7 @@ function Router() {
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/mypage" element={<MyPage />} />
         <Route path="*" element={<>notFound</>} />
       </Routes>
     </BrowserRouter>
