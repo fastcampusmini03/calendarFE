899203d4e28523d705e7ecf1f588ac055da12182
dokimion24 <leecy3610@gmail.com>
2023-05-06 21:01:24 +0900

    ✨ return 값 userInfo state 추가

--- src/hooks/useVerifyUser.ts
+++ src/hooks/useVerifyUser.ts
@@ -5,14 +5,17 @@ import { getCookie } from '../utils/cookies'
 
 type authType = 'PENDING' | 'SUCCESS' | 'FAILED'
 
-export const useVerifyUser = () => {
+export const useVerify = () => {
   const [isAuthenticated, setIsAuthenticated] = useState<authType>('PENDING')
+  const [userInfo, setUserInfo] = useState()
 
   const token = getCookie('accessToken')
 
-  const { data: userInfo } = useQuery(['user'], verify, {
-    onSuccess: () => {
+  const { data } = useQuery(['user'], verify, {
+    staleTime: 60 * 60 * 1000, //accessToken 만료 시간과 동일한 시간 적용해야됨.
+    onSuccess: (data) => {
       setIsAuthenticated('SUCCESS')
+      setUserInfo(data.payload.user)
     },
     onError: () => {
       setIsAuthenticated('FAILED')
