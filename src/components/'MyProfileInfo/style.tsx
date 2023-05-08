5225dceb84c972e91f28f78867ff65ed2f341cb4
dokimion24 <leecy3610@gmail.com>
2023-05-08 17:03:26 +0900

    💄 style 수정

--- src/components/'MyProfileInfo/style.tsx
+++ src/components/'MyProfileInfo/style.tsx
@@ -1,9 +1,15 @@
-import TextField from '@mui/material/TextField'
 import { styled } from '@mui/system'
+import { TextField, Box } from '@mui/material'
 
 export const Input = styled(TextField)`
   & input {
     height: 20px;
     padding: 8px;
   }
+`
+
+export const ErrorMessage = styled(Box)`
+  color: red;
+  font-size: 12px;
+  height: 12px;
 `
