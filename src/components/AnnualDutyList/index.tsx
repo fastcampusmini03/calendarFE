ccb65659e86e1b9d71deb7e17e86d944a1dd8e75
jyc-coder <jyc4648@gmail.com>
2023-05-09 17:46:41 +0900

    :lipstick: 관리자 페이지가 스낵바 추가
    관리자가  당직/연차에 대한 요청을 승인/거절 할때, 좌측 하단에 스낵바가 표시되게 구현
    
    - 요청에 따른 시간을 고려해서 0.5뒤에 스낵바가 표시되게 구현
    
    TODO
    - 스낵바를 표시하는 기능 메소드 내부에는 반드시 api 호출코드를 추가할 것

--- src/components/AnnualDutyList/index.tsx
+++ src/components/AnnualDutyList/index.tsx
@@ -7,7 +7,14 @@ import { Box, Button } from '@mui/material'
 import Tab from '@mui/material/Tab'
 import Tabs from '@mui/material/Tabs'
 import Container from '@mui/material/Container'
+import Dialog from '@mui/material/Dialog'
+import DialogTitle from '@mui/material/DialogTitle'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogActions from '@mui/material/DialogActions'
 import { useState } from 'react'
+import { justifyContent } from 'styled-system'
+import Toast from '../Common/Toast'
 
 interface AdminPageProps {
   saveDates: DatesPayload[]
@@ -17,6 +24,34 @@ interface AdminPageProps {
 
 function AnnualDutyList({ saveDates, editDates, deleteDates }: AdminPageProps) {
   const [value, setValue] = useState('approve')
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const [refuseOpen, setRefuseOpen] = useState(false)
+  const [accToastOpen, setAccToastOpen] = useState(false)
+  const [decToastOpen, setDecToastOpen] = useState(false)
+  const toggleDialog = () => {
+    setDialogOpen((prev) => !prev)
+  }
+  const toggleRefuseDialog = () => {
+    setRefuseOpen((prev) => !prev)
+  }
+
+  const approveDate = () => {
+    setDialogOpen((prev) => !prev)
+    setTimeout(() => {
+      setAccToastOpen((prev) => !prev)
+    }, 500) // 0.5초 후에 스낵바를 활성화
+
+    //TODO value 값에 따라 동작하는 api 메소드를 다르게 설정하여 추가할것 (승인,수정,삭제)
+  }
+
+  const declineDate = () => {
+    setRefuseOpen((prev) => !prev)
+
+    setTimeout(() => {
+      setDecToastOpen((prev) => !prev)
+    }, 500) // 0.5초 후에 스낵바를 활성화
+    //TODO value 값에 따라 동작하는 api 메소드를 다르게 설정하여 추가할것(승인, 수정, 삭제)
+  }
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue)
   }
@@ -106,9 +141,39 @@ function AnnualDutyList({ saveDates, editDates, deleteDates }: AdminPageProps) {
                         </Container>
 
                         <Box sx={{ display: 'flex', flexDirection: 'row', gap: '10px' }}>
-                          <Button>승인</Button>
-                          <Button variant="outlined">거부</Button>
+                          <Button onClick={toggleDialog}>승인</Button>
+                          <Button variant="outlined" onClick={toggleRefuseDialog}>
+                            거부
+                          </Button>
                         </Box>
+                        <Dialog open={dialogOpen}>
+                          <DialogTitle id="alert-dialog-title">{'권한 변경'}</DialogTitle>
+                          <DialogContent>
+                            <DialogContentText id="alert-dialog-description">
+                              승인하시겠습니까?
+                            </DialogContentText>
+                          </DialogContent>
+                          <DialogActions sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <Button onClick={approveDate}>예</Button>
+                            <Button autoFocus onClick={toggleDialog}>
+                              아니오
+                            </Button>
+                          </DialogActions>
+                        </Dialog>
+                        <Dialog open={refuseOpen}>
+                          <DialogTitle id="alert-dialog-title">{'권한 변경'}</DialogTitle>
+                          <DialogContent>
+                            <DialogContentText id="alert-dialog-description">
+                              거부하시겠습니까?
+                            </DialogContentText>
+                          </DialogContent>
+                          <DialogActions sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <Button onClick={declineDate}>예</Button>
+                            <Button autoFocus onClick={toggleRefuseDialog}>
+                              아니오
+                            </Button>
+                          </DialogActions>
+                        </Dialog>
                       </ListItem>
                     </>
                   )}
@@ -151,9 +216,39 @@ function AnnualDutyList({ saveDates, editDates, deleteDates }: AdminPageProps) {
                     </Container>
 
                     <Box sx={{ display: 'flex', flexDirection: 'row', gap: '10px' }}>
-                      <Button>승인</Button>
-                      <Button variant="outlined">거부</Button>
+                      <Button onClick={toggleDialog}>승인</Button>
+                      <Button variant="outlined" onClick={toggleRefuseDialog}>
+                        거부
+                      </Button>
                     </Box>
+                    <Dialog open={dialogOpen}>
+                      <DialogTitle id="alert-dialog-title">{'권한 변경'}</DialogTitle>
+                      <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                          승인하시겠습니까?
+                        </DialogContentText>
+                      </DialogContent>
+                      <DialogActions sx={{ display: 'flex', justifyContent: 'center' }}>
+                        <Button onClick={approveDate}>예</Button>
+                        <Button autoFocus onClick={toggleDialog}>
+                          아니오
+                        </Button>
+                      </DialogActions>
+                    </Dialog>
+                    <Dialog open={refuseOpen}>
+                      <DialogTitle id="alert-dialog-title">{'권한 변경'}</DialogTitle>
+                      <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                          거부하시겠습니까?
+                        </DialogContentText>
+                      </DialogContent>
+                      <DialogActions sx={{ display: 'flex', justifyContent: 'center' }}>
+                        <Button onClick={declineDate}>예</Button>
+                        <Button autoFocus onClick={toggleRefuseDialog}>
+                          아니오
+                        </Button>
+                      </DialogActions>
+                    </Dialog>
                   </ListItem>
                 </>
               ))
@@ -194,15 +289,55 @@ function AnnualDutyList({ saveDates, editDates, deleteDates }: AdminPageProps) {
                     </Container>
 
                     <Box sx={{ display: 'flex', flexDirection: 'row', gap: '10px' }}>
-                      <Button>승인</Button>
-                      <Button variant="outlined">거부</Button>
+                      <Button onClick={toggleDialog}>승인</Button>
+                      <Button variant="outlined" onClick={toggleRefuseDialog}>
+                        거부
+                      </Button>
                     </Box>
+                    <Dialog open={dialogOpen}>
+                      <DialogTitle id="alert-dialog-title">{'권한 변경'}</DialogTitle>
+                      <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                          승인하시겠습니까?
+                        </DialogContentText>
+                      </DialogContent>
+                      <DialogActions sx={{ display: 'flex', justifyContent: 'center' }}>
+                        <Button onClick={approveDate}>예</Button>
+                        <Button autoFocus onClick={toggleDialog}>
+                          아니오
+                        </Button>
+                      </DialogActions>
+                    </Dialog>
+                    <Dialog open={refuseOpen}>
+                      <DialogTitle id="alert-dialog-title">{'권한 변경'}</DialogTitle>
+                      <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                          거부하시겠습니까?
+                        </DialogContentText>
+                      </DialogContent>
+                      <DialogActions sx={{ display: 'flex', justifyContent: 'center' }}>
+                        <Button onClick={declineDate}>예</Button>
+                        <Button autoFocus onClick={toggleRefuseDialog}>
+                          아니오
+                        </Button>
+                      </DialogActions>
+                    </Dialog>
                   </ListItem>
                 </>
               ))
           }
         })()}
       </List>
+      <Toast
+        isOpened={accToastOpen}
+        handleClose={() => setAccToastOpen(false)}
+        message={`승인되었습니다!`}
+      />
+      <Toast
+        isOpened={decToastOpen}
+        handleClose={() => setDecToastOpen(false)}
+        message={`거부되었습니다!`}
+      />
     </div>
   )
 }
