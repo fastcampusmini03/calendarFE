e69c39cabbf52d1d2f5b3a20e0e856d5d477d23c
dokimion24 <leecy3610@gmail.com>
2023-05-13 03:26:10 +0900

    ✨  DateView 컴포넌트 생성

---
+++ src/components/DateView/index.tsx
@@ -0,0 +1,21 @@
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { useState } from 'react'
+
+const DateView = ({ handleChangeDate, setDate }) => {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DatePicker', 'DatePicker', 'DatePicker']}>
+        <DatePicker
+          onChange={(e) => handleChangeDate(e.$y, e.$M)}
+          label={'"month" and "year"'}
+          views={['month', 'year']}
+        />
+      </DemoContainer>
+    </LocalizationProvider>
+  )
+}
+
+export default DateView
