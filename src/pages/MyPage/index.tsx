32ee1346ab6a6467d853d6fe323a10e0678f5784
dokimion24 <leecy3610@gmail.com>
2023-05-13 03:32:43 +0900

    ✨ 스타일 컴포넌트 적용

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,32 +1,70 @@
 // import React, { useEffect, useState } from 'react'
-// import PersistentDrawerRight from '../../components/Header'
+import PersistentDrawerRight from '../../components/Header'
 
 import useVerify from '../../hooks/useVerify'
 import MyprofileInfo from '../../components/MyProfileInfo'
 
-// import { Container } from '@mui/material'
+import { Container } from '@mui/material'
+import MyDutyInfo from '../../components/MyDutyInfo'
+import {
+  getAllCalendarDates,
+  getCalendarDates,
+  getDeleteDates,
+  getEditDates,
+  getSaveDates,
+  getUserDuty,
+} from '../../apis/axios'
+import { useEffect } from 'react'
+import { ApproveData, CalendarData, DeleteData, EditData } from 'src/types/dates'
+import { useQuery } from 'react-query'
 // import { getUserDuty } from '../../apis/axios'
 
 const MyPage = () => {
   const { userInfo, isAuthenticated } = useVerify()
+  // const { data: calendarDates } = useQuery<CalendarData[]>('dates', getCalendarDates)
+  // const { data: AllcalendarDates } = useQuery<CalendarData[]>('Alldates', getAllCalendarDates)
+  // console.log({ AllcalendarDates })
+
+  // console.log( calendarDates )
+
+  // const approvedData = calendarDates?.filter((item) => item.status === '1')
+
+  // const userApprovedData = approvedData?.filter((item) => item.user.email === userInfo.email)
+  // console.log({ userApprovedData })
+
+  // const userDutys = filteredData
+  //   ?.map((item) => item.user)
+  //   .filter((item) => item.email === userInfo.email)
+
+  // console.log(calendarDates)
+  // const filteredUser = filteredData?.filter((item) => item.email)
 
   // useEffect(async () => {
-  //   const res = await getUserDuty()
+
+  //   const res = await getSaveDates()
   //   console.log({ res })
   // }, [])
 
-  console.log({ userInfo })
-  console.log({ isAuthenticated })
+  // const { data: saveDates } = useQuery<ApproveData>('saveDates', getSaveDates)
+  // const { data: editDates } = useQuery<EditData>('editDates', getEditDates)
+  // const { data: deleteDates } = useQuery<DeleteData>('deleteDates', getDeleteDates)
+  // const { data: userDuty } = useQuery('userDuty', getUserDuty)
+
+  // console.log({ deleteDates })
+  // console.log({ editDates })
+  // console.log({ saveDates })
+
+  // console.log({ userInfo })
+  // console.log({ isAuthenticated })
 
   return (
-    <MyprofileInfo userInfo={userInfo} />
-    // <>
-    //   <PersistentDrawerRight>
-    //     <Container maxWidth="md">
-
-    //     </Container>
-    //   </PersistentDrawerRight>
-    // </>
+    <>
+      <PersistentDrawerRight>
+        <Container maxWidth="md">
+          <MyprofileInfo userInfo={userInfo} />
+        </Container>
+      </PersistentDrawerRight>
+    </>
   )
 }
 
