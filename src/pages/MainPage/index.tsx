fc1479a019487a34f392c40ec236556cceb18801
jyc-coder <jyc4648@gmail.com>
2023-05-14 01:57:07 +0900

    :lipstick: Footer 컴포넌트 생성
    - Home (클릭시 메인 페이지로 이동) ,  ABOUT US
     CONTACT
    - Copyright
    - 조원 이름 작성

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,30 +1,32 @@
 import { useQuery } from 'react-query'
-import { CalendarData  } from '../../types/dates'
-import { useEffect, useState } from "react"
-import CalendarUI from "../../components/CalendarUI"
-import {  getCalendarDates } from "../../apis/axios"
+import { CalendarData } from '../../types/dates'
+import { useEffect, useState } from 'react'
+import CalendarUI from '../../components/CalendarUI'
+import { getCalendarDates } from '../../apis/axios'
 import PersistentDrawerRight from '../../components/Header'
-
-
-
+import Footer from '../../components/Footer'
 
 function MainPage() {
   /**calendar next/prev 버튼 기능 */
-const [year, setYear] = useState(2023)
-const [month, setMonth] = useState(5)
-console.log(year)
-console.log(month)
-// const queryClient = useQueryClient();
-  const { data: calendarDates, refetch } = useQuery<CalendarData[] | any>('dates', () => getCalendarDates({year, month})  
+  const [year, setYear] = useState(2023)
+  const [month, setMonth] = useState(5)
+  console.log(year)
+  console.log(month)
+  // const queryClient = useQueryClient();
+  const { data: calendarDates, refetch } = useQuery<CalendarData[] | any>('dates', () =>
+    getCalendarDates({ year, month }),
   )
   useEffect(() => {
-    refetch();
-  }, [year,month]);
-  
+    refetch()
+  }, [year, month])
+
   return (
-    <PersistentDrawerRight>
-      <CalendarUI view="month" dates={calendarDates} setYear={setYear} setMonth={setMonth}/>
-    </PersistentDrawerRight>
+    <>
+      <PersistentDrawerRight>
+        <CalendarUI view="month" dates={calendarDates} setYear={setYear} setMonth={setMonth} />
+      </PersistentDrawerRight>
+      <Footer />
+    </>
   )
 }
 
