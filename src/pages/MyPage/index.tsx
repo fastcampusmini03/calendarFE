7b1ec757e84501a1c1ea52da98e979d245321155
dokimion24 <leecy3610@gmail.com>
2023-05-14 18:07:23 +0900

    ✨ mypage 탭 추가

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,24 +1,38 @@
-// import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import PersistentDrawerRight from '../../components/Header'
 
 import useVerify from '../../hooks/useVerify'
 import MyprofileInfo from '../../components/MyProfileInfo'
 
-import { Container, Box } from '@mui/material'
+import { Container, Box, Tabs, Tab } from '@mui/material'
 import MyDutyInfo from '../../components/MyDutyInfo'
 
 const MyPage = () => {
   const { userInfo } = useVerify()
+  const [value, setValue] = useState('personalInfo')
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue)
+  }
 
   return (
     <>
       <PersistentDrawerRight>
-        <Container maxWidth="md">
-          <Box sx={{ height: '100vh' }}>
-            <MyprofileInfo userInfo={userInfo!} />
-            <MyDutyInfo userInfo={userInfo!} />
+        <Box sx={{ display: 'flex' }}>
+          <Box sx={{ borderRight: '1px solid #808080', width: '15%', height: '100vh' }}>
+            <Tabs orientation="vertical" value={value} onChange={handleChange}>
+              <Tab label="개인정보 수정" value="personalInfo" sx={{ fontSize: '20px' }} />
+              <Tab label="신청 내역 확인" value="dutyInfo" sx={{ fontSize: '20px' }} />
+            </Tabs>
           </Box>
-        </Container>
+          <Box ml={10} mt={3} sx={{ width: '60%' }}>
+            {value === 'personalInfo' ? (
+              <MyprofileInfo userInfo={userInfo!} />
+            ) : (
+              <MyDutyInfo userInfo={userInfo!} />
+            )}
+          </Box>
+        </Box>
       </PersistentDrawerRight>
     </>
   )
