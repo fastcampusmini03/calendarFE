bb18604f4fa2095b42ac5154e1525ecd2a3fa9b9
dokimion24 <leecy3610@gmail.com>
2023-05-15 14:15:29 +0900

    ✨ admin 계정 로그인시 /admin 이동

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,22 +4,27 @@ import { login, signup } from '../apis/axios'
 import { APIResponses } from '../types/response'
 // import { setCookie } from '../utils/cookies'
 import { useNavigate } from 'react-router-dom'
+import useVerify from './useVerify'
 
 export const useAuth = () => {
   const navigate = useNavigate()
   const queryClient = useQueryClient()
   const [isOpened, setIsOpened] = useState(false)
   const [userName, setUserName] = useState('')
+  const { userInfo } = useVerify()
 
   const successLogin = (data: APIResponses) => {
     setUserName(data.data.username)
     setIsOpened((prev) => !prev)
+
+    if (data.data.role === 'ADMIN') {
+      navigate('/admin')
+    }
   }
 
   const { mutate: loginUser, isError } = useMutation(login, {
     onSuccess: (data) => {
       if (!data) return
-      console.log({ data })
       successLogin(data)
     },
   })
