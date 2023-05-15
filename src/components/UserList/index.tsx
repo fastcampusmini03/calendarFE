136c174ddae3bfd703ef6049dcc70c460ed0a3da
jyc-coder <jyc4648@gmail.com>
2023-05-15 14:29:13 +0900

    :sparkles: 유저 리스트 무한 스크롤 구현
    - 연차 / 당직 관리와 같은 원리로 useInfiniteDates 훅에 useInfiniteUsers 를 추가
    - 유저 페이지에서 getUsers 일반 react-query를 받아오는 대신 useInfiniteUsers로 대체하여 fetchNexPAge, hasNextPage를 props로 전달
    
    
    유저페이지의 경우 상위에서 받아온 User를 serarchResult state로 적용해서  map을 통해 구현했으나, useInfiniteQuery 로 인해서 response의 형태가 변경되어 전반적으로 수정을 진행
    
    - searchResult  (검색어 텍스트)
    - handleInputChange (검색어 변경 인지 기능, debounce를 사용하여 0.5초 뒤에 검색어를 인식)
    
    - useMutate를 호출하고 onsuccess 일때 다시 유저 데이터 리스트를 호출
    
    - 무한 스크롤 구현을 위한 intersectionObserver 관련 코드 작성
    
    - sentinelRef가 존재할 경우, isIntersecting 이 true일때 fetchNextPage()를 호출하여 추가로 데이터를 가져온다.
    
    - 페이지에서 받아온 데이터는 infiniteData<UserData> 이므로 user.content 가 아니라 pages.map 에 존재하는 각각의 page 내부에 page.content 가 존재하기 때문에 map 메소드 코드를 변경
    
    - searchResult로 filter를 진행해서 page.content.filter를 사용해서 data.username에서  searchResult를 includes 하는 데이터만 보여줄수 있도록 설정함
    
    - 검색어가 없으면 전체 결과를 보여줄수 있게 설정함

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -2,46 +2,50 @@ import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import Stack from '@mui/material/Stack'
-
 import SearchIcon from '@mui/icons-material/Search'
 import { ListPaper, Search, SearchIconWrapper, StyledInputBase } from '../../style/style'
 import Popover from '@mui/material/Popover'
 import RadioGroup from '@mui/material/RadioGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Radio from '@mui/material/Radio'
-import { useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import Dialog from '@mui/material/Dialog'
 import DialogTitle from '@mui/material/DialogTitle'
 import DialogContent from '@mui/material/DialogContent'
 import DialogContentText from '@mui/material/DialogContentText'
 import DialogActions from '@mui/material/DialogActions'
 import Toast from '../Common/Toast'
-import { User, UserData } from '../../types/user'
+import { UserData } from '../../types/user'
 import { updateRole } from '../../apis/axios'
-import { useMutation, useQueryClient } from 'react-query'
+import { InfiniteData, useMutation, useQueryClient } from 'react-query'
 import CssBaseline from '@mui/material/CssBaseline'
 import Container from '@mui/material/Container'
-
+import { debounce } from 'lodash'
 interface UserPageProps {
-  users: UserData
+  users: InfiniteData<UserData>
+  fetchNextUser: () => void
+  hasNextUserPage: boolean
 }
 
-export default function UserList({ users }: UserPageProps) {
+type FetchParmas = {
+  fetchNextPage: () => void
+}
+
+export default function UserList({ users, fetchNextUser, hasNextUserPage }: UserPageProps) {
   /** 검색 결과 state */
-  const [searchResult, setSearchResult] = useState<User[]>(users.content)
-  console.log(searchResult)
+  const [searchResult, setSearchResult] = useState('')
+
   /** toast 컴포넌트 팝업 토글 */
   const [toastToggle, setToastToggle] = useState(false)
 
   /** input에 입력된 값에 따라  해당 유저의 이름과 일치하는 유저 리스트 데이터를 설정*/
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const input = event.target.value
-    if (input === '') {
-      setSearchResult(users.content)
-    } else {
-      setSearchResult(users.content.filter((data) => data.username === input))
-    }
-  }
+  const handleInputChange = useCallback(
+    debounce((event: React.ChangeEvent<HTMLInputElement>) => {
+      const input = event.target.value
+      setSearchResult(input)
+    }, 500),
+    [],
+  )
 
   /** popper 생성 위치 기준점 state */
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
@@ -59,12 +63,9 @@ export default function UserList({ users }: UserPageProps) {
     {
       onSuccess: () => {
         queryClient.invalidateQueries('users', { exact: true }).then(() => {
-          const queryData = queryClient.getQueryData<UserData>('users')
-          if (queryData) {
-            const updatedUsers = queryData.content
+          queryClient.refetchQueries('users', { exact: true }).then(() => {
             setAnchorEl(null)
-            setSearchResult(updatedUsers)
-          }
+          })
         })
       },
     },
@@ -104,6 +105,34 @@ export default function UserList({ users }: UserPageProps) {
   }
   // 리스트를 클릭했을 때에만 팝업이 나타나게 설정
   const open = Boolean(anchorEl)
+
+  /** IO */
+  const useInfiniteScroll = ({ fetchNextPage }: FetchParmas) => {
+    const sentinelRef = useRef(null)
+
+    useEffect(() => {
+      if (sentinelRef.current) {
+        const observer = new IntersectionObserver(
+          ([entry]) => {
+            if (entry.isIntersecting) {
+              fetchNextPage()
+            }
+          },
+          { root: null, rootMargin: '0px 0px 0px 0px', threshold: 0 }, // 센티널 엘리먼트가 화면에 노출되기 이전에 fetchNextPage()를 호출하도록 rootMargin을 지정합니다.
+        )
+
+        observer.observe(sentinelRef.current)
+
+        return () => {
+          observer.disconnect()
+        }
+      }
+    }, [sentinelRef, fetchNextPage])
+
+    return sentinelRef
+  }
+
+  const sentinelRefUser = useInfiniteScroll({ fetchNextPage: fetchNextUser })
 
   return (
     <div
@@ -157,60 +186,134 @@ export default function UserList({ users }: UserPageProps) {
 
         <Box sx={{ width: '80%', justifyItems: 'center' }}>
           <Stack spacing={2}>
-            {searchResult.map((data) => (
-              <div key={data.id}>
-                <ListPaper onClick={(event) => popupToggle(event)}>
-                  <Typography variant="h5">
-                    {data.username} ({data.email}) {data.role === 'USER' ? '일반' : '관리자'}
-                  </Typography>
-                </ListPaper>
-                <Popover
-                  open={open}
-                  anchorEl={anchorEl}
-                  onClose={handleClose}
-                  anchorOrigin={{
-                    vertical: 'center',
-                    horizontal: 'right',
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                  }}
-                >
-                  <Box sx={{ padding: '10px' }}>
-                    <Typography variant="h6">권한을 선택하세요</Typography>
-                    <Box
-                      sx={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                      }}
-                    >
-                      <RadioGroup value={value} onChange={editUserRole}>
-                        <FormControlLabel value="USER" control={<Radio />} label="일반" />
-                        <FormControlLabel value="ADMIN" control={<Radio />} label="관리자" />
-                      </RadioGroup>
-                      <Button onClick={dialogToggle}>변경</Button>
-                      <Dialog open={dialogOpen}>
-                        <DialogTitle id="alert-dialog-title">{'권한 변경'}</DialogTitle>
-                        <DialogContent>
-                          <DialogContentText id="alert-dialog-description">
-                            '{data.username}'의 권한을 {value === 'USER' ? '일반' : '관리자'}(으)로
-                            변경하시겠습니까?
-                          </DialogContentText>
-                        </DialogContent>
-                        <DialogActions>
-                          <Button onClick={editRole(data.email, value)}>예</Button>
-                          <Button onClick={dialogToggle} autoFocus>
-                            아니오
-                          </Button>
-                        </DialogActions>
-                      </Dialog>
-                    </Box>
-                  </Box>
-                </Popover>
-              </div>
-            ))}
+            {searchResult === ''
+              ? users.pages.map((page) =>
+                  page.content.map((data) => (
+                    <div key={data.id}>
+                      <ListPaper onClick={(event) => popupToggle(event)}>
+                        <Typography variant="h5">
+                          {data.username} ({data.email}) {data.role === 'USER' ? '일반' : '관리자'}
+                        </Typography>
+                      </ListPaper>
+                      <Popover
+                        open={open}
+                        anchorEl={anchorEl}
+                        onClose={handleClose}
+                        anchorOrigin={{
+                          vertical: 'center',
+                          horizontal: 'right',
+                        }}
+                        transformOrigin={{
+                          vertical: 'top',
+                          horizontal: 'left',
+                        }}
+                      >
+                        <Box sx={{ padding: '10px' }}>
+                          <Typography variant="h6">권한을 선택하세요</Typography>
+                          <Box
+                            sx={{
+                              display: 'flex',
+                              flexDirection: 'row',
+                              justifyContent: 'space-between',
+                            }}
+                          >
+                            <RadioGroup value={value} onChange={editUserRole}>
+                              <FormControlLabel value="USER" control={<Radio />} label="일반" />
+                              <FormControlLabel value="ADMIN" control={<Radio />} label="관리자" />
+                            </RadioGroup>
+                            <Button onClick={dialogToggle}>변경</Button>
+                            <Dialog open={dialogOpen}>
+                              <DialogTitle id="alert-dialog-title">{'권한 변경'}</DialogTitle>
+                              <DialogContent>
+                                <DialogContentText id="alert-dialog-description">
+                                  '{data.username}'의 권한을 {value === 'USER' ? '일반' : '관리자'}
+                                  (으)로 변경하시겠습니까?
+                                </DialogContentText>
+                              </DialogContent>
+                              <DialogActions>
+                                <Button onClick={editRole(data.email, value)}>예</Button>
+                                <Button onClick={dialogToggle} autoFocus>
+                                  아니오
+                                </Button>
+                              </DialogActions>
+                            </Dialog>
+                          </Box>
+                        </Box>
+                      </Popover>
+                    </div>
+                  )),
+                )
+              : users.pages.map((page) =>
+                  page.content
+                    .filter((data) => data.username.includes(searchResult))
+                    .map((data) => (
+                      <div key={data.id}>
+                        <ListPaper onClick={(event) => popupToggle(event)}>
+                          <Typography variant="h5">
+                            {data.username} ({data.email}){' '}
+                            {data.role === 'USER' ? '일반' : '관리자'}
+                          </Typography>
+                        </ListPaper>
+                        <Popover
+                          open={open}
+                          anchorEl={anchorEl}
+                          onClose={handleClose}
+                          anchorOrigin={{
+                            vertical: 'center',
+                            horizontal: 'right',
+                          }}
+                          transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                          }}
+                        >
+                          <Box sx={{ padding: '10px' }}>
+                            <Typography variant="h6">권한을 선택하세요</Typography>
+                            <Box
+                              sx={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                justifyContent: 'space-between',
+                              }}
+                            >
+                              <RadioGroup value={value} onChange={editUserRole}>
+                                <FormControlLabel value="USER" control={<Radio />} label="일반" />
+                                <FormControlLabel
+                                  value="ADMIN"
+                                  control={<Radio />}
+                                  label="관리자"
+                                />
+                              </RadioGroup>
+                              <Button onClick={dialogToggle}>변경</Button>
+                              <Dialog open={dialogOpen}>
+                                <DialogTitle id="alert-dialog-title">{'권한 변경'}</DialogTitle>
+                                <DialogContent>
+                                  <DialogContentText id="alert-dialog-description">
+                                    '{data.username}'의 권한을{' '}
+                                    {value === 'USER' ? '일반' : '관리자'}
+                                    (으)로 변경하시겠습니까?
+                                  </DialogContentText>
+                                </DialogContent>
+                                <DialogActions>
+                                  <Button onClick={editRole(data.email, value)}>예</Button>
+                                  <Button onClick={dialogToggle} autoFocus>
+                                    아니오
+                                  </Button>
+                                </DialogActions>
+                              </Dialog>
+                            </Box>
+                          </Box>
+                        </Popover>
+                      </div>
+                    )),
+                )}
+            {hasNextUserPage ? (
+              <div ref={sentinelRefUser}>감지중</div>
+            ) : (
+              <Typography variant="h4" align="center">
+                마지막 페이지입니다
+              </Typography>
+            )}
           </Stack>
         </Box>
         {/*권한 변경 confirm시 나타나는 Toast*/}
--- src/pages/UserListPage/index.tsx
+++ src/pages/UserListPage/index.tsx
@@ -3,14 +3,20 @@ import { getUsers } from '../../apis/axios'
 import UserList from '../../components/UserList'
 // import { DatesPayload } from '../../types/dates'
 import { UserData } from '../../types/user'
+import { useInfiniteUsers } from '../../hooks/useInfiniteDates'
 
 function UserListPage() {
-  const { data: users, isLoading } = useQuery<UserData>('users', getUsers)
+  const {
+    data: users,
+    fetchNextPage: fetchNextUser,
+    isLoading,
+    hasNextPage: hasNextUserPage,
+  } = useInfiniteUsers()
 
-  if (isLoading || users === undefined) {
+  if (isLoading || users === undefined || hasNextUserPage === undefined) {
     return <div>로딩중...</div>
   }
-  return <UserList users={users} />
+  return <UserList users={users} fetchNextUser={fetchNextUser} hasNextUserPage={hasNextUserPage} />
 }
 
 export default UserListPage
