253c124b38ace1b6c4c2e657c9a7770f13cb994f
jyc-coder <jyc4648@gmail.com>
2023-05-17 18:01:30 +0900

    :lipstick: 푸터 스타일 수정
    조원 이름이 적힌 typography를 하나로 합쳐서 한줄로 가운데 정렬

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,9 +4,20 @@ import Typography from '@mui/material/Typography'
 import TwitterIcon from '@mui/icons-material/Twitter'
 import FacebookIcon from '@mui/icons-material/Facebook'
 import InstagramIcon from '@mui/icons-material/Instagram'
+import { useLocation } from 'react-router-dom'
 export default function Footer() {
+  const location = useLocation()
+
+  console.log(location.pathname)
+
+  const positionStyle: React.CSSProperties =
+    location.pathname === '/login' ||
+    location.pathname === '/signup' ||
+    location.pathname === '/admin/user'
+      ? { position: 'fixed', bottom: 0, width: '100%' }
+      : {}
   return (
-    <>
+    <div style={positionStyle}>
       <CssBaseline />
       <div style={{ width: '100%', height: '1px', border: '1px solid black' }} />
       <Box
@@ -36,27 +47,33 @@ export default function Footer() {
               margin: '20px 0 ',
             }}
           >
-            <Typography variant="h4">
+            <Typography variant="h5" sx={{ fontWeight: 600 }}>
               <a style={{ textDecoration: 'none', color: '#000' }} href="/">
                 Home
               </a>
             </Typography>
-            <Typography variant="h4">ABOUT US</Typography>
-            <Typography variant="h4">CONTACT</Typography>
+            <Typography variant="h5" sx={{ fontWeight: 600 }}>
+              ABOUT US
+            </Typography>
+            <Typography variant="h5" sx={{ fontWeight: 600 }}>
+              CONTACT
+            </Typography>
           </Box>
 
-          <Typography variant="h4" sx={{ display: 'flex', gap: '20px', marginBottom: '20px' }}>
+          <Typography variant="h5" sx={{ paddingBottom: '10px' }}>
+            BE : 유현주, 김태헌, 류동우, 정재은 FE : 정영찬, 신환수, 이찬영
+          </Typography>
+
+          <Typography variant="h5" sx={{ paddingBottom: '10px' }}>
+            Copyright @ 2023 AduCalendar All rights reserved
+          </Typography>
+          <Typography variant="h5" sx={{ display: 'flex', gap: '20px', marginBottom: '20px' }}>
             <TwitterIcon />
             <FacebookIcon />
             <InstagramIcon />
           </Typography>
-          <Typography variant="h6">Copyright @ 2023 AduCalendar All rights reserved</Typography>
-        </Box>
-        <Box>
-          <Typography variant="h6">BE : 유현주, 김태헌, 류동우, 정재은</Typography>
-          <Typography variant="h6">FE : 정영찬, 신환수, 이찬영</Typography>
         </Box>
       </Box>
-    </>
+    </div>
   )
 }
